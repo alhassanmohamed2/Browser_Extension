ae9f5270211aca2b0519f040eb60ef34a110afc2
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2022-08-22 05:26:37 +0200

    update

--- Twitter Trend Section/content.js
+++ Twitter Trend Section/content.js
@@ -1,17 +1,13 @@
 // Removes Twitter Trend Section
 
-function checkDOMChange() {
-    var twitter_feed = document.querySelectorAll(".r-g2wdr4");
+setInterval(() => {
+    var twitter_feed = document.querySelectorAll(" .r-vacyoi ")
     if (twitter_feed.length != 0) {
 
         twitter_feed[0].outerHTML = "";
-        twitter_feed[1].outerHTML = "";
-
 
 
     } else {
         setTimeout(checkDOMChange, 100);
     }
-}
-
-checkDOMChange();
+}, 1000);
